2f9c9ac859be61b61b27686d0d874702209c5f82
xwenliang <wenliang.web@gmail.com>
2016-06-15 17:31:06 +0800

    support es2015

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var babel = require('babel-core');
+var es2015 = require('babel-preset-es2015');
 
 module.exports = function (content, file, opts) {
     file.isES6 = (file.isES6 == undefined) ? true : file.isES6;
     if (!file.isES6) return content;
     opts.moduleId = file.getId();
+    opts.presets = opts.presets || es2015;
     var result = babel.transform(content, opts);
     return result.code;
 };
